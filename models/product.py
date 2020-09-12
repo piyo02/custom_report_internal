83492dc8e64a2ed6ecb548e68ac9286c3f0545a5
piyo02 <alzidni2000@gmail.com>
2020-09-12 22:58:51 +0800

    push

---
+++ models/product.py
@@ -0,0 +1,18 @@
+from odoo import api, fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    canvas_price = fields.Float(string="Canvas Price", compute="get_canvas_price")
+
+    @api.one
+    def get_canvas_price(self):
+        canvas_pricelist = self.env['product.pricelist'].search([('name', '=', 'Harga Kampas')])
+
+        for line in canvas_pricelist.item_ids:
+            product = self.env['product.template'].search([('name', '=', line.name)])
+            product.canvas_price = line.fixed_price
+            
